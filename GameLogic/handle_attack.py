dc6362cabda5d88eefd45da2b366d927312e6e0e
KimrAndrew <andrewrk1997@gmail.com>
2022-02-03 21:45:23 -0800

    fixed bug where last pokemon getting knocked out raised an exception

--- GameLogic/handle_attack.py
+++ GameLogic/handle_attack.py
@@ -13,11 +13,11 @@ def calculations(game, attacking_user, targeted_user, selection):
     move = attacker['moves'][selection]
     updated_target = attack(attacker,target,move)
     if updated_target['stats']['hp'] <= 0:
-        if len(pokemon_list) > 0:
+        if len(pokemon_list) > 1:
             pokemon_list.pop(0)
             game[f'active_{targeted_user}_pokemon'] = pokemon_list[0]
         else:
             # No More Pokemon :(
-            return
+            game[f'active_{targeted_user}_pokemon'] = None
     else:
         target['stats']['hp'] == updated_target['stats']['hp']
